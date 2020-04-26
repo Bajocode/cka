3fd79c4f62f58adf39c792b1812c5db11102e062
Fabijan Bajo <bajo09@gmail.com>
2020-04-26 21:20:45 +0200

    init

---
+++ up.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# vim
+git clone --depth=1 https://github.com/amix.git/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# kubectl
+apt-get install bash-completion -y
+kubectl completion bash
+
+# bashrc
+cat <<EOF >> ~/.bashrc
+
+# CKA
+
+# completion
+source <(kubectl completion bash)
+kubectl completion bash >/etc/bash_completion.d/kubectl
+alias k=kubectl
+complete -F __start_kubectl k
+
+# alias k8s
+alias kg='kubectl get'
+alias ke='kubectl explain'
+alias ker='kubectl explain --recursive'
+alias kd='kubectl describe'
+alias ka='kubectl apply -f'
+alias kdl='kubectl delete'
+alias krk='kubectl run --help | grep kubectl'
+alias krr='kubectl run --help | grep restart'
+alias kga='kubectl get all'
+alias kgp='kubectl get pods'
+alias kgn='kubectl get nodes'
+alias kgd='kubectl get deployments'
+alias kge='kubectl get events'
+alias kgs='kubectl get services'
+
+# alias ubuntu
+alias jxe='journalctl -xe'
+alias jxu='journalctl -xe -u'
+alias ss='systemctl status'
+alias sr='systemctl restart'
+EOF
+
+# reload
+exec $SHELL && source ~/.bashrc
